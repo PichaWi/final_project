07f77c2e7a450dea1b76e873ff3eb6ec1bd72f77
Picha Wiwattanawongsa <picha.w@ku.th>
2023-12-16 15:17:23 +0700

    Add all the thing to the role what they can do.

--- project_manage.py
+++ project_manage.py
@@ -71,15 +71,113 @@ if __name__ == "__main__":
     login_table = tables.search_table('login')
     val = login(login_table)
 
+    def manage_database(tables):
+        print("Database tables loaded:", [table.get_table_name() for table in tables.tables])
+
+        # Example: Display details of each table
+        for table in tables.tables:
+            print(f"\nDetails for {table.get_table_name()}:")
+            print(table.table_data)
+
+        # Example: Update details in a table
+        table_name_to_update = input("Enter the name of the table you want to update: ")
+
+        # Assuming the existence of an 'update_table' function in your Table class
+        if tables.search_table(table_name_to_update):
+            print(f"Current data for {table_name_to_update}:")
+            print(tables.search_table(table_name_to_update).table_data)
+
+            # Get updated data
+            updated_data = input("Enter the updated data (comma-separated values): ")
+            updated_data_list = updated_data.split(',')
+
+            # Assuming the existence of an 'update_table' function in your Table class
+            tables.search_table(table_name_to_update).update_table(updated_data_list)
+            print(f"{table_name_to_update} updated successfully.")
+        else:
+            print(f"Table {table_name_to_update} not found.")
+
+
+    def faculty_activities(tables):
+        # Assuming 'Project' is the name of the project table
+        project_table = tables.search_table('Project')
+
+        # See requests to be a supervisor
+        supervisor_requests = project_table.get_supervisor_requests()
+        print("Supervisor requests:")
+        print(supervisor_requests)
+        response_to_supervisor_request = input("Enter your response to a supervisor request (accept/deny): ")
+        # Assuming the existence of a 'respond_to_supervisor_request' function in your Table class
+        project_table.respond_to_supervisor_request(response_to_supervisor_request)
+
+        # Evaluate a project
+        project_id_to_evaluate = input("Enter the ID of the project you want to evaluate: ")
+        evaluation_score = input("Enter your evaluation score (out of 10): ")
+        # Assuming the existence of an 'evaluate_project' function in your Table class
+        project_table.evaluate_project(project_id_to_evaluate, evaluation_score)
+
+        # Approve a project
+        project_id_to_approve = input("Enter the ID of the project you want to approve: ")
+        # Assuming the existence of an 'approve_project' function in your Table class
+        project_table.approve_project(project_id_to_approve)
+
+        print("Faculty activities completed.")
+
 
 # based on the return value for login, activate the code that performs activities according to the role defined for that person_id
 
     if val and val[1] == 'admin':
-        pass
+        manage_database(tables)
 #see and do admin related activities
     elif val and val[1] == 'student':
-        pass
-#see and do student related activities
+        student_table = tables.search_table('persons')
+        student_id = val[0]
+
+        # 1. See if there are pending requests to become members of already created projects
+        member_requests = tables.search_table('member_pending_request').filter(lambda x: x['member_id'] == student_id)
+
+        if member_requests:
+            print("Pending requests to become members:")
+            print(member_requests.table_data)
+
+            # 2. Accept or deny the requests
+            for request in member_requests.table_data:
+                decision = input(f"Do you want to accept the request for project {request['project_id']}? (yes/no): ")
+
+                if decision.lower() == 'yes':
+                    # Update Member_pending_request table (remove the accepted request)
+                    tables.search_table('member_pending_request').table_data.remove(request)
+
+                    # Update Project table (set member status to 'accepted')
+                    project_table = tables.search_table('project')
+                    project_id = request['project_id']
+                    project_table.update_entry('ID', project_id, lambda x: x['ID'] == project_id,
+                                               {'member_status': 'accepted'})
+
+                    print(f"Request for project {project_id} accepted.")
+                else:
+                    # Update Member_pending_request table (remove the denied request)
+                    tables.search_table('member_pending_request').table_data.remove(request)
+                    print(f"Request for project {request['project_id']} denied.")
+
+        # 3. If more members needed, send out requests and update the member_pending_request table
+        # Requests can only go to those whose role is student, i.e., not yet become a member or a lead
+        project_needs_members = tables.search_table('project').filter(lambda x: x['member_status'] == 'needed')
+
+        for project in project_needs_members.table_data:
+            if project['lead_id'] == student_id:
+                # Skip projects where the student is already a lead
+                continue
+
+            # Check if the student is not yet a member
+            if not any(member['project_id'] == project['ID'] for member in student_table.table_data):
+                # Add a request to Member_pending_request table
+                new_member_request = {'project_id': project['ID'], 'member_id': student_id}
+                tables.search_table('member_pending_request').insert_entry(new_member_request)
+
+                print(f"Request sent to join project {project['ID']}.")
+
+    #see and do student related activities
     elif val and val[1] == 'member':
         member_table = tables.search_table('persons')
 
@@ -110,13 +208,88 @@ if __name__ == "__main__":
 
 #see and do member related activities
     elif val and val[1] == 'lead':
-        pass
-#see and do lead related activities
+        # see and do lead related activities
+        lead_project_id = tables.search_table('persons')
+
+        # 1. See project status (pending member, pending advisor, or ready to solicit an advisor)
+        status = tables.search_table('project').get_project_status(lead_project_id)
+        print(f"Project status: {status}")
+
+        # 2. See and modify project information
+        # Display current project information
+        print("Current project information:")
+        print(tables.search_table('project').filter(lambda x: x['ID'] == lead_project_id).table_data)
+
+        # Modify project information
+        new_information = {'description': 'Updated description', 'deadline': '2023-12-31'}
+        success = tables.search_table('project').modify_project_information(lead_project_id, new_information)
+
+        if success:
+            print("Project information modified successfully")
+        else:
+            print("Project not found")
+
+        # 3. See who has responded to the requests sent out
+        # Get responses to requests
+        responses = tables.search_table('project').get_responses_to_requests(lead_project_id)
+        print(f"Responses to requests: {responses}")
+
+        # 4. Send out requests to potential members (Member_pending_request table needs to be updated)
+        # Assuming 'member_pending_request' is the table for member pending requests
+        member_pending_request_table = tables.search_table('member_pending_request')
+
+        # Update Member_pending_request table with new requests
+        new_member_requests = [{'project_id': lead_project_id, 'member_id': '456'},
+                               {'project_id': lead_project_id, 'member_id': '789'}]
+        member_pending_request_table.table_data.extend(new_member_requests)
+
+        # 5. Send out requests to a potential advisor (Advisor_pending_request table needs to be updated)
+        # Assuming 'advisor_pending_request' is the table for advisor pending requests
+        advisor_pending_request_table = tables.search_table('advisor_pending_request')
+
+        # Update Advisor_pending_request table with a new request
+        new_advisor_request = {'project_id': lead_project_id, 'advisor_id': '101'}
+        advisor_pending_request_table.table_data.append(new_advisor_request)
+
+    #see and do lead related activities
     elif val and val[1] == 'faculty':
-        pass
+        faculty_activities(tables)
     #see and do faculty related activities
     elif val and val[1] == 'advisor':
-        pass
+        project_table = tables.search_table('Project')
+
+        # Send a request to be a supervisor
+        supervisor_request = input("Enter your request to be a supervisor: ")
+        # Assuming the existence of a 'send_supervisor_request' function in your Table class
+        project_table.send_supervisor_request(supervisor_request)
+
+        # Send request to see the current work and situation of a project
+        project_id_to_view = input("Enter the ID of the project you want to view: ")
+        # Assuming the existence of a 'send_project_view_request' function in your Table class
+        project_table.send_project_view_request(project_id_to_view)
+
+        # Send accept or deny response to lead
+        lead_response = input("Enter your response to the lead (accept/deny): ")
+        # Assuming the existence of a 'send_lead_response' function in your Table class
+        project_table.send_lead_response(lead_response)
+
+        # See details of all projects
+        all_projects = project_table.get_all_projects_details()
+        print("Details of all projects:")
+        print(all_projects)
+
+        # Evaluate a project
+        project_id_to_evaluate = input("Enter the ID of the project you want to evaluate: ")
+        evaluation_score = input("Enter your evaluation score (out of 10): ")
+        # Assuming the existence of an 'evaluate_project' function in your Table class
+        project_table.evaluate_project(project_id_to_evaluate, evaluation_score)
+
+        # Approve a project
+        project_id_to_approve = input("Enter the ID of the project you want to approve: ")
+        # Assuming the existence of an 'approve_project' function in your Table class
+        project_table.approve_project(project_id_to_approve)
+
+        print("Advisor activities completed.")
 #see and do advisor related activities
 
 # once everything is done, make a call to the exit function
